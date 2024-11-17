ffdd2ac45bd0401fb6be7f705072f8b51f61d597
Naga Adithya Vuppalapati <nagaadithyavuppalapati@gmail.com>
2024-11-17 13:09:49 -0700

    [Staff page creation]

---
+++ EntertainmentApp/Staff.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EntertainmentApp
+{
+    public partial class Staff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnListMembers_Click(object sender, EventArgs e) { }
+        protected void btnListStaff_Click(object sender, EventArgs e) { }
+        protected void btnAddStaff_Click(object sender, EventArgs e) { }
+    }
+}
