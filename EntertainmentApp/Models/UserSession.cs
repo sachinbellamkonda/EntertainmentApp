03d9d1d84a5612420541c17f84ed8b9c3f433e94
Sachin Bellamkonda <sachinbellamkonda1@gmail.com>
2024-11-16 20:37:17 -0700

    Implemented membeLogin page and captcha generation in member login page. Also included the login cookies code for member login page

---
+++ EntertainmentApp/Models/UserSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntertainmentApp.Models
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+        public string SessionId { get; set; }
+        public string UserType { get; set; }
+    }
+
+    public class Response
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public UserSession CurrentSession { get; set; }
+    }
+}
